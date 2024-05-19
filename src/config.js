db3f31a17e729c25edad6e15bb9ee56ef582f17d
kiyohken2000 <www.broadwell.com@gmail.com>
2024-05-19 15:12:04 +0900

    master

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { googleSheetKey } from "./openaiKeys"
 
-const versionName = '1.0.35.1'
-const iosBuildNumber = '42'
+const versionName = '1.0.36'
+const iosBuildNumber = '43'
 
 const sheetId = '15OtDRuaSXWhZ8odAHi0E7Pn93hzvO3gvDIYSpopNngY'
 const sheetName = 'sheet2'
