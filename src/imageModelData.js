382da9044850686a6a9b29821aa3a77875a32702
kiyohken2000 <www.broadwell.com@gmail.com>
2024-07-01 15:47:18 +0900

    master

--- src/imageModelData.js
+++ src/imageModelData.js
@@ -3,7 +3,7 @@ const imageModelData = {
     label: 'RealisticVision',
     sequence: 1,
     negativePromptKey: 'negativePromptRealisticVision',
-    url: 'https://api-inference.huggingface.co/models/SG161222/Realistic_Vision_V1.4',
+    url: 'https://api-inference.huggingface.co/models/SG161222/Realistic_Vision_V2.0',
     recommendNegativePrompt: '(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck',
   },
   Animagine: {
